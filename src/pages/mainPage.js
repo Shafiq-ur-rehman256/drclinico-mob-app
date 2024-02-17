4bdc6ae8253e31f055c4c75f676fa09ef6324a54
onaisahmed <onaisahmed02@gmail.com>
2024-02-18 03:22:41 +0500

    Added inbox screen UI

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,142 +1,149 @@
-import { StyleSheet, Text, View, Image, Pressable, TextInput } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Entypo,FontAwesome5  } from '@expo/vector-icons';
-import emergencyHome from './emergencyHome';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  TextInput,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, FontAwesome5 } from "@expo/vector-icons";
+import emergencyHome from "./emergencyHome";
+import inbox from "./inbox";
 
 function HomeScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
 function SettingsScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
 export default Mainpage = () => {
-    // Tab.Navigator.defaultProps.screenOptions.headerShown = false;
-    
-    return (
-        <Tab.Navigator
-        screenOptions={({ route }) => ({
-            tabBarShowLabel: false,
-            headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let Size;
-            console.log(route);
-            console.log(color, size);
-            if (route.name === 'Home1') {
-              iconName = focused ? 'ios-home' : 'ios-home-outline';
-              Size = size
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'md-document-text' : 'md-document-text-outline';
-              Size = size
-            }
-            else if (route.name === 'Settings1') {
-                iconName = focused ? 'md-scan-circle' : 'md-scan-circle-outline';
-                Size = size + 20
+  // Tab.Navigator.defaultProps.screenOptions.headerShown = false;
 
-              }
-
-            else if (route.name === 'Settings2') {
-                iconName = focused ? 'chatbubble-ellipses' : 'chatbubble-ellipses-outline';
-                Size = size
-
-            }
-            else if (route.name === 'Settings3') {
-                iconName = focused ? 'user-alt' : 'user';
-                Size = size
-
-              }
-            return route.name === 'Settings3' ?  <FontAwesome5  name={iconName} size={Size} color={color} /> : <Ionicons name={iconName} size={Size} color={color} />;
-          },
-          tabBarActiveTintColor: '#2FC1FF',
-          tabBarInactiveTintColor: '#2FC1FF',
-        })}
-      >
-        <Tab.Screen name="Home1" component={emergencyHome} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="Settings1" component={SettingsScreen} />
-        <Tab.Screen name="Settings2" component={SettingsScreen} />
-        <Tab.Screen name="Settings3" component={SettingsScreen} />
-      </Tab.Navigator>
-    )
-
-}
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let Size;
+          console.log(route);
+          console.log(color, size);
+          if (route.name === "Home1") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+            Size = size;
+          } else if (route.name === "Settings") {
+            iconName = focused
+              ? "md-document-text"
+              : "md-document-text-outline";
+            Size = size;
+          } else if (route.name === "Settings1") {
+            iconName = focused ? "md-scan-circle" : "md-scan-circle-outline";
+            Size = size + 20;
+          } else if (route.name === "Settings2") {
+            iconName = focused
+              ? "chatbubble-ellipses"
+              : "chatbubble-ellipses-outline";
+            Size = size;
+          } else if (route.name === "Settings3") {
+            iconName = focused ? "user-alt" : "user";
+            Size = size;
+          }
+          return route.name === "Settings3" ? (
+            <FontAwesome5 name={iconName} size={Size} color={color} />
+          ) : (
+            <Ionicons name={iconName} size={Size} color={color} />
+          );
+        },
+        tabBarActiveTintColor: "#2FC1FF",
+        tabBarInactiveTintColor: "#2FC1FF",
+      })}
+    >
+      <Tab.Screen name="Home1" component={emergencyHome} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Settings1" component={SettingsScreen} />
+      <Tab.Screen name="Settings2" component={inbox} />
+      <Tab.Screen name="Settings3" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        color: 'black',
-        justifyContent: 'center',
-        backgroundColor: '#fff'
-    },
-    logoContainer: {
-        marginTop: 50
-    },
-    logo: {
-        width: 40, // Set the desired width
-        height: 40, // Set the desired height
-        resizeMode: 'contain' // Adjust the resizeMode based on your preference
-    },
-    heading: {
-        // flex: 1,
-        alignItems: 'center',
-        marginTop: 40,
-        rowGap: 10,
-        paddingLeft: 10,
-        paddingRight: 10,
-
-    },
-    mainHeading: {
-        fontFamily: 'Gilroy-SemiBold',
-        fontSize: 30,
-        textAlign: 'center'
-    },
-    primaryHeading: {
-        color: '#A7A6A5',
-        textAlign: 'center',
-        fontSize: 18,
-    },
-    underlineStyleBase: {
-        width: 50,
-        height: 50,
-        borderWidth: 0,
-        backgroundColor: '#EFF2F1',
-        color: '#1C1F1E',
-        fontWeight: '900',
-        fontSize: 20
-        // borderBottomWidth: 1,
-    },
-    bottomBtn: {
-        marginTop: 40
-    },
-    bottomText: {
-        display: 'flex',
-        flexDirection: 'row',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        columnGap: 5,
-        width: '80%',
-        marginTop: 20,
-    },
-    part1: {
-        color: '#A7A6A5',
-        fontSize: 16
-    },
-    part2: {
-        color: '#2FC1FF',
-        fontSize: 16
-    },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    color: "black",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  logoContainer: {
+    marginTop: 50,
+  },
+  logo: {
+    width: 40, // Set the desired width
+    height: 40, // Set the desired height
+    resizeMode: "contain", // Adjust the resizeMode based on your preference
+  },
+  heading: {
+    // flex: 1,
+    alignItems: "center",
+    marginTop: 40,
+    rowGap: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  mainHeading: {
+    fontFamily: "Gilroy-SemiBold",
+    fontSize: 30,
+    textAlign: "center",
+  },
+  primaryHeading: {
+    color: "#A7A6A5",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  underlineStyleBase: {
+    width: 50,
+    height: 50,
+    borderWidth: 0,
+    backgroundColor: "#EFF2F1",
+    color: "#1C1F1E",
+    fontWeight: "900",
+    fontSize: 20,
+    // borderBottomWidth: 1,
+  },
+  bottomBtn: {
+    marginTop: 40,
+  },
+  bottomText: {
+    display: "flex",
+    flexDirection: "row",
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    columnGap: 5,
+    width: "80%",
+    marginTop: 20,
+  },
+  part1: {
+    color: "#A7A6A5",
+    fontSize: 16,
+  },
+  part2: {
+    color: "#2FC1FF",
+    fontSize: 16,
+  },
+});
