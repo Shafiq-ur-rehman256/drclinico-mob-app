3d4f9e9fa008cf31ba90bc90034cbb63a1877520
onaisahmed <onaisahmed02@gmail.com>
2024-03-15 00:12:30 +0500

    Added route for doctor pages navigation

---
+++ src/layouts/doctor.Navigation.js
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import patientHome from "../pages/Patient/patientHome";
+import patientDonation from "../pages/Patient/patientDontation";
+
+const Stack = createNativeStackNavigator();
+
+export default function PatientNavigation() {
+  return (
+    <Stack.Navigator
+      initialRouteName="doctorStack"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="patient_home" component={patientHome} />
+      <Stack.Screen name="patient_donate" component={patientDonation} />
+    </Stack.Navigator>
+  );
+}
